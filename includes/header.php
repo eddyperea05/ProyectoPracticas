99772fc34738faa09d833626fd056360f82c8e96
Eddy Perea <eddyperea05@gmail.com>
2022-05-21 15:19:41 -0500

    Integración de estilos

--- includes/header.php
+++ includes/header.php
@@ -4,9 +4,247 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reto</title>
+    <title>QUICK ACCESS</title>
+
+    <style>
+        body {
+            font-family: 'Roboto Mono', monospace;
+
+        }
+
+        .login-desing {
+            display: grid;
+            grid-template-columns: repeat(2, 50%);
+            height: 100vh;
+        }
+
+        .waves {
+            background-image: url("../images/wave.png");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .login {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .login-data {
+
+            text-align: center;
+            width: 60%;
+
+
+
+        }
+
+        .login-data h1 {
+
+            font-weight: 200;
+            text-align: center;
+
+        }
+
+        .input-group {
+            position: relative;
+            width: 60%;
+            margin: 0 auto;
+            margin-bottom: 1.5rem;
+
+        }
+
+        .input-group2 {
+            position: relative;
+            width: 60%;
+            margin: 0 auto;
+            margin-bottom: 1.5rem;
+
+        }
+
+        .input-field input {
+            border: none;
+            border-bottom: 3px solid #062726;
+            width: 100%;
+            height: 2rem;
+            font-size: 1.0625rem;
+            padding-left: 2rem;
+            line-height: 147.6%;
+            padding-top: 1rem;
+            padding-bottom: 0.5rem;
+
+        }
+
+        .input-field input:focus {
+            outline: none;
+            border-color: #6247AA;
+        }
+
+        .input-field input:hover {
+            background-color: #E2CFEA;
+
+
+        }
+
+        .input-field input:focus+.input-label {
+
+            top: 0;
+            margin-bottom: 2rem;
+            color: #6247AA;
+        }
+
+        .input-field input:focus~i {
+            color: #6247AA;
+        }
+
+        .input-field .input-label {
+            position: absolute;
+            top: 15px;
+            left: 28px;
+            line-height: 147.6%;
+            color: #062726;
+            transition: top .2s;
+            font-weight: 700;
+        }
+
+        .input-field2 .input-label2 {
+            border: none;
+
+            width: 100%;
+            height: 2rem;
+            font-size: 1.0625rem;
+            padding-left: 2rem;
+            line-height: 147.6%;
+            padding-top: 1rem;
+            padding-bottom: 0.5rem;
+
+        }
+
+        .input-field2 i {
+            position: absolute;
+            top: 2px;
+            left: 60px;
+            color: #062726;
+            font-size: 1.5rem;
+        }
+
+        .input-field i {
+            position: absolute;
+            top: 19px;
+            left: 0;
+            color: #062726;
+            font-size: 1.5rem;
+        }
+
+        .login-form a {
+            display: block;
+            width: 85%;
+            text-align: right;
+            text-decoration: none;
+            color: #062726;
+            margin-bottom: 1.3rem;
+            font-weight: 700;
+            transition: .3s ease-in-out;
+            font-size: 18px;
+        }
+
+        .login-form a:hover {
+
+            color: #A06CD5;
+        }
+
+        .btn {
+
+            border-radius: 3rem;
+            width: 45%;
+            border: 1px solid #A06CD5;
+            line-height: 1.5rem;
+            font-size: 1rem;
+            background-color: #A06CD5;
+            color: #fff;
+            padding: 0.5rem 0;
+            transition: .3s ease-in-out all;
+            font-weight: 500;
+
+        }
+
+        .btn:hover {
+
+            cursor: pointer;
+            background-color: #E2CFEA;
+            color: #062726;
+        }
+
+        #menu ul li {
+            cursor: pointer;
+            background-color: #fff;
+            color: #062726;
+        }
+
+        #menu ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        #menu ul a {
+            border-radius: 3rem;
+            width: 100%;
+            line-height: 1.5rem;
+            font-size: 1rem;
+            background-color: #fff;
+            padding: 0.5rem 0;
+            transition: .3s ease-in-out all;
+            font-weight: 500;
+            color: #062726;
+            text-decoration: none;
+            font-weight: 400;
+            font-size: 15px;
+            padding: 10px;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+        }
+
+        #menu ul li {
+            position: relative;
+            float: left;
+            margin: 0;
+            padding: 0;
+        }
+
+        #menu ul li:hover {
+            background: #fff;
+        }
+
+        #menu ul ul {
+            display: none;
+            position: absolute;
+            top: 100%;
+            left: 0;
+            padding: 0;
+        }
+
+        #menu ul ul li {
+            float: none;
+            width: 150px
+        }
+
+        #menu ul ul a {
+            line-height: 120%;
+            padding: 10px 15px;
+        }
+
+        #menu ul li:hover>ul {
+            display: block;
+        }
+    </style>
+
     <!--BOOTSTRAP 4 -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">-->
     <!--QR-->
     <script src="https://unpkg.com/qrious@4.0.2/dist/qrious.js"></script>
 
@@ -24,8 +262,22 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark bg-dark">
+    <!--<nav class="navbar navbar-dark bg-dark">
         <div class="container">
+            <?php if (isset($_SESSION['Idusuario'])) { ?>
+                <a href="inicio.php" class="navbar-brand">Inicio</a>
+                <?php if ($_SESSION['Tipouser'] == '0') { ?>
+                    <a href="usuario.php" class="navbar-brand">Gestionar de Usuarios</a>
+                <?php } ?>
+                <!--<a href="producto.php" class="navbar-brand">Administrar Productos</a>-0->
+                <a href="cerrarSesion.php"><i class="fas fa-power-off"></i></a>
+            <?php }; ?>
+
+        </div>
+    </nav>-->
+    <main class="login-desing">
+    <nav class="">
+        <div class="">
             <?php if (isset($_SESSION['Idusuario'])) { ?>
                 <a href="inicio.php" class="navbar-brand">Inicio</a>
                 <?php if ($_SESSION['Tipouser'] == '0') { ?>
--- index.php
+++ index.php
@@ -1,42 +1,46 @@
 <?php include('config/database.php'); ?>
 <?php include('includes/header.php'); ?>
-<main class="login-desing">
-    <div class="waves">
-        <img src="../img/maecita-removebg-preview.png" alt="">
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 mx-auto">
-                <?php if (isset($_SESSION['message'])) { ?>
-                    <div class="alert alert-<?php echo $_SESSION['message_type']; ?> alert-dismissible fade show" role="alert">
-                        <?php echo $_SESSION['message'] ?>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                <?php
-                    unset($_SESSION['message']);
-                    unset($_SESSION['message_type']);
-                } ?>
-                <div class="card card-body">
-                    <h2>Login</h2>
-                    <br>
-                    <form action="ingreso.php" method="POST">
-                        <div class="form-group">
-                            <input type="text" name="Login" placeholder="Por favor ingresar login" class="form-control" autofocus>
-                        </div>
-                        <br>
-                        <div class="form-group">
-                            <input type="password" name="Passwd" placeholder="Por favor ingrese contraseña" class="form-control">
-                        </div>
-                        <br>
-                        <div class="col text-center">
-                            <input type="submit" name="Ingresar" class="btn btn-success btn-block" value="Ingresar Usuario">
-                        </div>
-                    </form>
+
+<div class="waves">
+    <img src="../images/maecita-removebg-preview.png" alt="">
+</div>
+<div class="login">
+    <div class="login-data">
+        <!--<div class="">-->
+            <?php if (isset($_SESSION['message'])) { ?>
+                <div class="alert alert-<?php echo $_SESSION['message_type']; ?> alert-dismissible fade show" role="alert">
+                    <?php echo $_SESSION['message'] ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-            </div>
-        </div>
+            <?php
+                unset($_SESSION['message']);
+                unset($_SESSION['message_type']);
+            } ?>
+            <!--<div class="">-->
+                <h1>Login</h1>
+                <br>
+                <form action="ingreso.php" method="POST" class="login-form">
+                    <div class="input-group">
+                        <label class="input-field">
+                            <input type="text" name="Login" placeholder="Por favor ingresar login" class="form-control" autofocus>
+                        </label>
+                    </div>
+                    <br>
+                    <div class="input-group">
+                        <label class="input-field">
+                            <input type="password" name="Passwd" placeholder="Por favor ingrese contraseña" class="form-control">
+                        </label>
+                    </div>
+                    <br>
+                    <div class="col text-center">
+                        <input type="submit" name="Ingresar" class="btn" value="Ingresar Usuario">
+                    </div>
+                </form>
+            <!--</div>-->
+        <!--</div>-->
     </div>
-</main>
+</div>
+
 <?php include('includes/footer.php'); ?>
